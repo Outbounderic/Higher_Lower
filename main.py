44e1139fe3773577f7538fc4040fb80376670828
Outbounderic <ericvanmeter57@gmail.com>
2021-12-29 14:58:21 -0600

    Created basic list for randomly selected data.

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 from art import logo, vs
 from game_data import data
+import random
+
+current_compare = []
+
+# Higher Lower game
+# From data randomly pick a dictionary
+# Present that information 
+# Randomly pick a second dictionary
+# Present that information but hide follower count
+# Ask for input on if the new info follower count is higher or lower
+# Compare the follower count and return wether the guess was correct or not
+# If correct, remove first dictionary and generate new dictionary to continue game loop
+# If not correct, display you lose and exit game
+
+def pick_data():
+    current_compare.append(data[random.randint(1, len(data))])
+
+pick_data()
+pick_data()
+
+print(current_compare)
